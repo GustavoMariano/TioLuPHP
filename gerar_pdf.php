97988285ed1d3d41235cf16ed3e648b6979e2a11
GustavoMariano <47873305+GustavoMariano@users.noreply.github.com>
2019-05-31 21:11:52 -0300

    Add files via upload

--- gerar_pdf.php
+++ gerar_pdf.php
@@ -1,18 +1,33 @@
 <?php	
+include 'conectaDB.php';
 
 	$html = "";
+	     $html .= '<h2>Dados dos Produtos</h2>';
+       $rs = $pdo->prepare("SELECT * FROM tb_produtos");
+
+       if ($rs->execute()) {
+         if ($rs->rowCount() > 0) {
+           $row = $rs->fetch(PDO::FETCH_OBJ);
+           $html .= "Código do Cliente : ".$row->id_produto . "<br>";
+           $html .= "Nome : ".$row->descricao. "<br>";
+           $html .= "Endereco : ".$row->valor. "<br>";
+           $html.= "<img src='{$row->imagem}' width='100' height='100' name='imagem' value='{$row->imagem}'>"."<br>";
+         }
+       }
+
 	//referenciar o DomPDF com namespace
 	use Dompdf\Dompdf;
 
 	// include autoloader
 	require_once("dompdf/autoload.inc.php");
 
+
 	//Criando a Instancia
 	$dompdf = new DOMPDF();
 
 	// Carrega seu HTML
-	$dompdf->load_html('
-			'.$html.'
+	$dompdf->load_html(' 
+		'.$html.'
 		');
 
 	//Renderizar o html
